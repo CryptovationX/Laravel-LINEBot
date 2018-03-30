02c400ae7d9e313355cf9df6fdfae4cdf2a5de1f
WorakornX <worakorn.wattanakulchart@gmail.com>
2018-03-31 03:03:59 +0700

    done live price

--- app/Http/Controllers/LivePriceController.php
+++ app/Http/Controllers/LivePriceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Redis;
 use Line;
 
 use Illuminate\Http\Request;
@@ -10,6 +11,58 @@ class LivePriceController extends Controller
 {
     public function test()
     {
-        Line::pushMessage('Ua2b3dd43fdfaf129015087ee98896a5a','hello i fai');
+        $orderbooks= array();
+        for ($i = 0; $i < 7 ; $i++) { 
+            for ($j = 0; $j < 4 ; $j++) { 
+                $orderbooks[$i][$j][0] = json_decode(Redis::get(''.$i.".".$j))[0];
+                $orderbooks[$i][$j][1] = json_decode(Redis::get(''.$i.".".$j))[1];
+                $orderbooks[$i]['exchange'] = Redis::get(''.$i.".e");
+            }
+        }
+
+        $message ="Bitcoin Price in USD (% vs Bx)\r\n";
+
+        $message .= $orderbooks[2]['exchange'].": ";
+        $message .= round($orderbooks[2][0][0],2)." (0%)\r\n";
+
+        foreach ($orderbooks as $key => $orderbook) {
+        
+            if ($key != 2) {
+                $message .= $orderbook['exchange'].": ";
+                $message .= round($orderbook[0][0],2)." (".round(($orderbook[0][0]-$orderbooks[2][0][0])/$orderbooks[2][0][0]*100,2)."%)\r\n";
+            }
+        };
+
+        $message .="\r\nEthereum Price in USD (% vs Bx)\r\n";
+
+        $message .= $orderbooks[2]['exchange'].": ";
+        $message .= round($orderbooks[2][1][0],2)." (0%)\r\n";
+
+        foreach ($orderbooks as $key => $orderbook) {
+        
+            if ($key != 2) {
+                $message .= $orderbook['exchange'].": ";
+                $message .= round($orderbook[1][0],2)." (".round(($orderbook[1][0]-$orderbooks[2][1][0])/$orderbooks[2][1][0]*100,2)."%)\r\n";
+            }
+        };
+
+        $message .="\r\nRipple Price in USD (% vs Bx)\r\n";
+
+        $message .= $orderbooks[2]['exchange'].": ";
+        $message .= round($orderbooks[2][2][0],4)." (0%)\r\n";
+
+        foreach ($orderbooks as $key => $orderbook) {
+        
+            if ($key != 0) {
+                if ($key != 2) {
+                    $message .= $orderbook['exchange'].": ";
+                    $message .= round($orderbook[2][0],4)." (".round(($orderbook[2][0]-$orderbooks[2][2][0])/$orderbooks[2][2][0]*100,2)."%)\r\n";
+                }
+            }
+        };
+
+        // echo($message);
+        // $json = json_encode($orderbooks);
+        Line::pushMessage('Ua2b3dd43fdfaf129015087ee98896a5a',$message);
     }
 }
